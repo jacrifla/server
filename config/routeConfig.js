763eb72cebc8628f62393ce6a48816578e0f3add
jacrifla <jaquelini_cris@hotmail.com>
2025-01-02 23:37:30 -0300

    removi

--- config/routeConfig.js
+++ config/routeConfig.js
@@ -6,7 +6,6 @@ const listRoutes = require('../routes/shoppingList');
 const listItemRoutes = require('../routes/listItem');
 const priceHistoryRoutes = require('../routes/priceHistory');
 const itemNotesRoutes = require('../routes/itemNotes');
-const shareListRoutes = require('../routes/shareList');
 const sharedListTokensRoutes = require('../routes/sharedListTokens');
 
 module.exports = (app) => {
@@ -18,6 +17,5 @@ module.exports = (app) => {
     app.use('/list-item', listItemRoutes);
     app.use('/price-history', priceHistoryRoutes);
     app.use('/item-notes', itemNotesRoutes);
-    app.use('/share-list', shareListRoutes);
     app.use('/shared-list-tokens', sharedListTokensRoutes);
 }
--- routes/shareList.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sharedListController = require('../controllers/shareList');
-
-router.post('/', sharedListController.shareList);
-router.get('/:user_id', sharedListController.getSharedLists);
-router.delete('/:shared_list_id', sharedListController.deleteSharedList);
-
-module.exports = router;
