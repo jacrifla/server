e17f9056b6687300716db05177fb70fc653d3046
jacrifla <jaquelini_cris@hotmail.com>
2025-01-19 19:58:46 -0300

    arquivos removidos

--- routes/itemNotes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const itemNotesController = require('../controllers/itemNotes');
-
-router.post('/', itemNotesController.createNote);
-router.get('/:itemId', itemNotesController.getNotes);
-router.put('/update', itemNotesController.updateNote);
-
-module.exports = router;
--- routes/priceHistory.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const priceHistoryController = require('../controllers/priceHistory');
-
-router.post('/', priceHistoryController.createPriceHistory);
-router.get('/:itemId', priceHistoryController.getPriceHistory);
-
-module.exports = router;
