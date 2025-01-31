b374d0a67a24dfbba0b2327edd18de128d4f3325
jacrifla <jaquelini_cris@hotmail.com>
2025-01-30 21:55:30 -0300

    fix

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 module.exports = (app) => {
     app.use(cors({
-        origin: '*',
+        origin: ['http://localhost:3000', 'https://listadamamae.netlify.app'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'] 
